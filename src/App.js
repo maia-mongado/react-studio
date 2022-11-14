7c936b5643954b41d77434ad23e551b1e1d77e0e
Maia Mongado <maiamongado@maias-mbp.devices.brown.edu>
2022-11-14 15:46:58 -0500

    done:

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
 import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem.js"
 
 /* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
 bakeryData.forEach((item) => {
@@ -11,18 +12,37 @@ bakeryData.forEach((item) => {
 function App() {
   // TODO: use useState to create a state variable to hold the state of the cart
   /* add your cart state code here */
+  const [cart, setCart] = useState({})
+  const [total, addItemCost] = useState(0)
+
+  const addToCart = (pastry) => {
+    const newCart = {...cart}
+    newCart[pastry.name] = (newCart[pastry.name] || 0) + 1
+    setCart(newCart)
+    addItemCost(total + pastry.price)
+  }
 
   return (
     <div className="App">
-      <h1>My Bakery</h1> {/* TODO: personalize your bakery (if you want) */}
 
-      {bakeryData.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-        <p>Bakery Item {index}</p> // replace with BakeryItem component
+      <div id="gallery">
+      <h1>My Bakery</h1> 
+      {bakeryData.map((item, index) => ( // DONE: map bakeryData to BakeryItem components
+        <p>
+          <BakeryItem pastry={item} onClick={addToCart}/>
+          <br></br>
+        </p>
       ))}
+      </div>
 
-      <div>
+      <div id="cart">
         <h2>Cart</h2>
-        {/* TODO: render a list of items in the cart */}
+        {Object.keys(cart).map((key, value) => ( /* DONE: render a list of items in the cart */
+        <p>
+          <li key={key}> {key} * {cart[key]}</li>
+        </p>
+        ))}
+        <p>Total: {total}</p>
       </div>
     </div>
   );
--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,18 @@
 // TODO: create a component that displays a single bakery item
+export default function BakeryItem(props) {
+    return(
+        <div>
+            <p>
+                <img src={props.pastry.image}></img>
+                <br></br>
+                {props.pastry.name}
+                <br></br>
+                {props.pastry.price}
+                <br></br>
+                {props.pastry.description}
+                <br></br>
+                <button onClick={() => {props.onClick(props.pastry)}}>Add to Cart</button>
+            </p>
+        </div>
+    )
+}
